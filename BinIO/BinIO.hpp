d8816319dcedab088ef483423e83270f4488092b
IonizingRadiation <PeterSmith_9@outlook.com>
2018-12-13 22:16:25 +0800

    BinIO modifies

--- BinIO/BinIO.hpp
+++ BinIO/BinIO.hpp
@@ -1,3 +1,8 @@
+#pragma once
+
+#ifndef BINIO_H
+#define BINIO_H
+
 #include <iostream>
 #include <fstream>
 #include <algorithm>
@@ -5,13 +10,13 @@
 #include <Eigen/Eigen>
 
 namespace ionizing {
-  using namespace Eigen;
-  using namespace std;
+  // using namespace std;
+  // using namespace Eigen;
 
   class BinIO {
   public:
     BinIO(const char* FileName);
-    ~BinIO() = default;
+    ~BinIO();
     BinIO(const BinIO&) = delete;             // Prevent 'copying' operations.
     BinIO& operator=(const BinIO&) = delete;
 
@@ -19,20 +24,71 @@ namespace ionizing {
     T readElement();
 
   template <typename T>
-    Matrix<T, Dynamic, 1> readVectorCol(const int size);
+    Eigen::Matrix<T, Eigen::Dynamic, 1> readVectorCol(const long size);
 
   template <typename T>
-    Matrix<T, 1, Dynamic> readVectorRow(const int size);
+    Eigen::Matrix<T, 1, Eigen::Dynamic> readVectorRow(const long size);
 
   template <typename T>
-    Matrix<T, Dynamic, Dynamic> readMatrix(const int nRow, const int nCol);
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> readMatrix(const long nRow, const long nCol);
 
   private:
-    ifstream ifs;
-    ofstream ofs;
+    std::ifstream ifs;
+    std::ofstream ofs;
 
+  private:
     void _openFile(const char* FileName);
+
+  }; // end of class BinIO
+
+/********************************************/
+/********  start of implementations  ********/
+/********************************************/
+
+  BinIO::BinIO(const char* FileName) {
+    _openFile(FileName);
+    std::cout << "BinIO created successful" << std::endl;
+  }
+
+  BinIO::~BinIO() {
+    ifs.close();
+    std::cout << "BinIO destructed" << std::endl;
+  }
+
+  void BinIO::_openFile(const char* FileName) {
+    ifs.open(FileName, std::ios::binary | std::ios::in);
+    std::string str_stars(10, '*');
+    if (ifs.fail()) {
+      std::cerr << str_stars << " Binary File \"" << FileName << "\" open failed " << str_stars << std::endl;
+      std::abort();
+    }
+  }
+
   template <typename T>
-    T _readElement();
-  };
+    T BinIO::readElement() {
+      long len = sizeof(T);
+      T x;
+      ifs.read((char*) &x, len);
+      return x;
+    }
+
+  template <typename T>
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> BinIO::readMatrix(const long nRow, const long nCol) {
+      Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> matrix;
+      matrix.resize(nRow, nCol);
+      ifs.read((char*) matrix.data(), nRow * nCol * sizeof(T));
+      return matrix;
+    }
+
+  template <typename T> 
+    Eigen::Matrix<T, Eigen::Dynamic, 1> BinIO::readVectorCol(const long size) {
+      return readMatrix<T>(size, 1);
+    }
+ 
+  template <typename T>
+    Eigen::Matrix<T, 1, Eigen::Dynamic> BinIO::readVectorRow(const long size) {
+      return readMatrix<T>(1, size);
+    } 
+
 };
+#endif  // BINIO_H
--- BinIO/README.md
+++ BinIO/README.md
@@ -4,3 +4,5 @@ Reqirements:
   - Eigen3 library
   - C++11 support
   - clang compiler
+
+**Matrix is column first stored in Eigen**
