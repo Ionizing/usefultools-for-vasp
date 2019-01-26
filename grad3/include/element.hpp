903b15aeb15b2951eec6621a6b7176b243a3cb7c
IonizingRadiation <PeterSmith_9@outlook.com>
2019-01-26 15:12:13 +0800

    [element] Modify Element Class

--- grad3/include/element.hpp
+++ grad3/include/element.hpp
@@ -9,6 +9,7 @@ namespace ionizing {
 struct Element {
   string Name;
   int    Num;
+  bool   isCart;
   MatX3d atomPos;
   MatX3d atomPosCart;
   MatX3d atomPosDire;
@@ -16,9 +17,12 @@ struct Element {
 
   Element(const char* name, const int num) : Name(name), Num(num) {}
   Element() : Element("", 0) {}
-  void init_with_cart(const int atom_num);
-  void init_with_dire(const int atom_num, 
-      const Mat33d lattice_vectors);
+  void init_with_cart(const int     atom_num,
+                      const MatX3d  atom_pos_cart,
+                      const VecStr  comments);
+  void init_with_dire(const int     atom_num, 
+                      const Mat33d  atom_pos_dire,
+                      const VecStr  comments);
 };
 
 };
